3b0fc4e88ef3597319173d84d9315df373e8baba
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-09-05 22:10:58 +0530

    chore

---
+++ README.md
@@ -0,0 +1,13 @@
+# FILE STRUCTURE
+- src
+  - components
+     - Steps
+        - Step1.jsx
+        - Step2.jsx
+        - Step3.jsx
+        - Step4.jsx
+     - Form.jsx
+  - App.jsx
+
+
+  
---
+++ src/components/Steps/Step1.jsx
@@ -0,0 +1,38 @@
+const Step1 = () => {
+  return(
+    <div>
+      <h2>
+        Create a project 
+      </h2>
+
+      <div>
+        {/* Project Name */}
+        <div>
+          <label>
+            Project Name
+          </label>
+          <input
+           type="text"
+           placeholder="Enter project name here"
+          />
+        </div>
+
+        {/* CLient section */}
+        <div>
+          <label>
+            Client
+          </label>
+          <div>
+            <selec>
+              
+            </selec>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+
+export default Step1;
